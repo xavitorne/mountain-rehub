82998e0124d0c4820a432053148ee3e9ed42d511
Xavi Torné <xavi@airpim.com>
2014-04-17 08:47:09 +0200

    Moved config to the root

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# if you have postgres use this uri:
+# SQLALCHEMY_DATABASE_URI = "postgresql://rehub:rehub@localhost/rehub"
+
+# if you want to use sqlite, use this one:
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'rehub.db')
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+DEBUG=True
+DATABASE=os.path.join(basedir, 'rehub.db')
+USERNAME='admin'
+PASSWORD='admin'
+SECRET_KEY='$cippalippa!'
+CSRF_ENABLED=True
--- rehub/config.py
+++
@@ -1,5 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "postgresql://rehub:rehub@localhost/rehub"
-
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
